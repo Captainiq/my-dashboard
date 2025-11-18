263393d2cc13a92201b00cbd47d64d8d52d3cec5
Muhammed Iqbal <35327462+Captainiq@users.noreply.github.com>
2025-11-19 01:47:29 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <Dashboard
-      spreadsheetId=2PACX-1vSSoZqPMIYN4AENE9Aeia7sMoXBYxlxftHViPAcYZ3fe_iXmLhRS3kmUJNv2Dl96Q
-      apiKey=921758913714-pbvqk1fqvu55k6klnlnst84ret46p5kr.apps.googleusercontent.com
+      spreadsheetId={spreadsheetId}
+      apiKey={apiKey}
       range="Sheet1!A1:I"
       pollIntervalMs={30000}
     />
